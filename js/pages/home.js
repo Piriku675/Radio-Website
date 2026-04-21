5445809b07491c7a5a0c4bb295e12a1cf5bd021d
Anthony Kuiau <akuiau@proton.me>
2026-04-21 20:55:48 +1000

    Add files via upload

---
+++ js/pages/home.js
@@ -0,0 +1,32 @@
+// ── HOME PAGE CONTROLLER ──────────────────────
+import {
+  getActivePromoStrip, getLiveStream, getFeaturedShow,
+  getLatestArticles, getTodaySchedule, getFacebookPosts,
+  getCurrentPromotion, getStationConfig
+} from "../data-service.js";
+import {
+  renderPromoStrip, renderCompactPlayer, renderCurrentShow,
+  renderHomeBlog, renderHomeSchedule,
+  renderPromoHighlight, renderFacebookFeed, triggerReveals
+} from "../renderers.js";
+
+export async function loadHomePage() {
+  const [promo, stream, show, articles, schedule, posts, promotion, config] = await Promise.all([
+    getActivePromoStrip(),
+    getLiveStream(),
+    getFeaturedShow(),
+    getLatestArticles(3),
+    getTodaySchedule(),
+    getFacebookPosts(2),
+    getCurrentPromotion(),
+    getStationConfig(),
+  ]);
+
+  renderPromoStrip(promo);
+  renderCompactPlayer(stream);
+  renderCurrentShow(show);
+  renderHomeBlog(articles, "home-blog-list");
+  renderHomeSchedule(schedule, show?.id);
+  renderFacebookFeed(posts, config?.facebook);
+  renderPromoHighlight(promotion);
+}
