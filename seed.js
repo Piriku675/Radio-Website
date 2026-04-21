38f65c15ad1993c56aea80f7f778071793358096
Anthony Kuiau <akuiau@proton.me>
2026-04-21 18:24:51 +1000

    Update seed.js

--- seed.js
+++ seed.js
@@ -15,12 +15,12 @@ import { getFirestore, setDoc, doc, Timestamp }    from "firebase/firestore";
 
 // ── PASTE YOUR FIREBASE CONFIG HERE ──────────────────────────
 const firebaseConfig = {
-  apiKey:            "YOUR_API_KEY",
-  authDomain:        "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId:         "YOUR_PROJECT_ID",
-  storageBucket:     "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId:             "YOUR_APP_ID"
+  apiKey: "AIzaSyC05Dzi0FatRWEVfc-bR7QzVHezGay6fI8",
+  authDomain: "radio-website-e18df.firebaseapp.com",
+  projectId: "radio-website-e18df",
+  storageBucket: "radio-website-e18df.firebasestorage.app",
+  messagingSenderId: "538244078719",
+  appId: "1:538244078719:web:f9a064f631378f497d734a"
 };
 
 const app = initializeApp(firebaseConfig);
